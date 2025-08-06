93ca021c502c319dddc1f85cf38dc44b24ffc049
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-08-06 23:43:30 +0700

    fix(auth): use absolute URL for Google OAuth callback

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -8,7 +8,7 @@ passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientSecret: process.env.GOOGLE_CALLBACK_URL,
       callbackURL: '/api/auth/google/callback',
       scope: ['profile', 'email'],
     },
