9b314cf38351229e8641685e9a2afb37a6c2e920
nuazsa <nurazissaputra@gmail.com>
2026-03-06 23:57:23 +0700

    chore(swagger): Add Swagger API documentation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import passport from 'passport';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { errorHandler } from './middlewares/error.middleware.js';
 import authRoutes from './routes/auth.route.js';
 import profileRoutes from './routes/profile.route.js';
@@ -8,10 +10,26 @@ import './config/passport.config.js';
 
 const app = express();
 
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mirach Community API',
+      version: '1.0.0',
+      description: 'Official REST API documentation for the Mirach Community platform.',
+    },
+  },
+  apis: ['./src/**/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
 app.use(cors());
 app.use(express.json());
 
 app.use(passport.initialize());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
 app.get('/', (req, res) => {
   return res.json({
